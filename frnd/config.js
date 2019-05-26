4db2d799f4959fd1d4236707a852cc9f9dbca861
Kyuseo Shim <cremazer@gmail.com>
2019-05-27 06:51:25 +0900

    test

--- frnd/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-	apiKey: 'AIzaSyBS7-4BS1S2QbOROmf6ZQO8SiSaGDIKnfw',
-	authDomain: 'reactnative-50697.firebaseapp.com',
-	databaseURL: 'https://reactnative-50697.firebaseio.com',
-	projectId: 'reactnative-50697',
-	storageBucket: 'reactnative-50697.appspot.com',
-	messagingSenderId: '689104383448'
-};
